b769994f8c3104078ec0e3abccc4f38dd3c3c7d7
yashkashyap04 <you@example.com>
2022-06-20 10:46:45 +0530

    additions

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -3,11 +3,55 @@ package Sorting;
 public class MergeSort {
     public static void main(String[] args) {
         int array [] = {8,2,5,3,1,4,9};
-        int merged [] = sort(array, 0, array.length - 1);
-        for (int i : merged) {
+        sort(array, 0, array.length - 1);
+        for (int i : array) {
             System.out.print(i + " ");
         }
     }
 
-    
+    public static void sort (int array[], int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            sort (array, start, mid);
+            sort (array, mid + 1, end);
+            merge(array, start, mid, end);
+        }
+    }
+
+    // 8,2,5,3,1,4,9
+    public static void merge (int array[], int start, int mid, int end) {
+        int leftSize = mid - start + 1;
+        int rightSize = end - mid;
+
+        int leftArray[] = new int[leftSize];
+        int rightArray[] = new int[rightSize];
+
+        for (int i = 0; i < leftSize; i++) {
+            leftArray[i] = array[start + i];
+        }
+
+        for (int i = 0; i < rightSize; i++) {
+            rightArray[i] = array[mid + 1 + i];
+        }
+
+        int i = 0, j = 0, k = start;
+        for (; i < leftSize && j < rightSize; k++) {
+            if (leftArray[i] <= rightArray[j])
+                array[k] = leftArray[i++];
+            else
+                array[k] = rightArray[j++];
+        }
+
+        while (i < leftSize) {
+            array[k] = leftArray[i];
+            k++;
+            i++;
+        }
+
+        while (j < rightSize) {
+            array[k] = rightArray[j];
+            k++;
+            j++;
+        }
+    }    
 }
--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -1,5 +1,44 @@
 package Sorting;
 
 public class QuickSort {
-    
+    public static void main(String[] args) {
+        int array [] = {8,2,5,3,1,4,9};
+        sort(array, 0, array.length - 1);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static int partition (int[] array, int start, int end) {
+        int pivot = array[start];
+        int i = start, j = end;
+        while (i < j) {
+            do {
+                i++;
+            } while (array[i] <= pivot);
+
+            do {
+                j--;
+            } while (array[j] > pivot);
+
+            if (i < j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[start];
+        array[start] = array[j];
+        array[j] = temp;
+        
+        return j;
+    }
+
+    public static void sort (int array[], int start, int end) {
+        if (start < end) {
+            int index = partition(array, start, end);
+            sort(array, start, index);
+            sort(array, index + 1, end);
+        }
+    }
 }
